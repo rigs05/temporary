4c3624e0d2d1346b8c569534a03f0124fa0023b8
rigs05 <bosco18494@gmail.com>
2023-10-24 12:49:21 +0530

    editing names

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()         // Invoking particular property from the
 router.post ('/', (req, res) => {
     const { name } = req.body
     if (name) {
-        return res.status(200).send (`Welcum ${name}`)
+        return res.status(200).send (`Welcome ${name}`)
     }
     res.status(404).send ('Invalid Credentials')
 })
